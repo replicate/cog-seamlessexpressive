f6777a0bef3576f14a00e4d557cb201496b722dc
tomtom <alaradirik@gmail.com>
2024-03-12 13:27:41 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# cog-seamlessexpressive
-Cog wrapper for SeamlessExpressive
+# Cog Wrapper for SeamlessExpressive
+
+SeamlessExpressive is a multilingual speech translation model that preserves the original vocal styles and prosody. It retains the nuances of speech, ensuring translations maintain the speaker's unique expressions and intonation. 
+
+See the original [paper](https://scontent-ist1-1.xx.fbcdn.net/v/t39.2365-6/406941874_247486308347770_2317832131512763077_n.pdf?_nc_cat=102&ccb=1-7&_nc_sid=3c67a6&_nc_ohc=w7nmCz1SmMkAX9G6vjb&_nc_ht=scontent-ist1-1.xx&oh=00_AfCjk9EB4gXYQZgbun4dzcRj5FEFiSJKf2tStwkQogBIHQ&oe=65EB8F69) and [model page](https://huggingface.co/facebook/seamless-expressive) and [repository](https://github.com/facebookresearch/seamless_communication) for more details.
+
+## How to use the API
+
+You need to have Cog and Docker installed to run this model locally. To build the docker container with cog and run a prediction:
+
+```
+cog predict -i audio_input=@sample.mp3 -i target_lang="German" -i duration_factor=1.1
+```
+
+To start a server and send requests to your locally or remotely deployed API:
+```
+cog run -p 5000 python -m cog.server.http
+```
+
+To translate and synthesize audio using SeamlessExpressive, you need to provide an audio file and specify your translation preferences. The API input arguments are as follows:
+
+- **audio_input:** Path to the input audio file that you want to translate and synthesize.  
+- **source_lang:** The original language of your input audio. Supported languages are English, French, Spanish, German, Italian and Chinese (Mandarin). Default value is English.  
+- **target_lang:** The target language for the output audio. Supported languages are English, French, Spanish, German, Italian and Chinese (Mandarin). Default value is French.  
+- **duration_factor:** Adjusts the timing to better match the target language's speech rhythm. Recommendations include 1.0 for English, Chinese, Spanish; 1.1 for German and Italian; and 1.2 for French. Default is 1.0.  
+
+## References
+```
+@inproceedings{seamless2023,
+   title="Seamless: Multilingual Expressive and Streaming Speech Translation",
+   author="{Seamless Communication}, Lo{\"i}c Barrault, Yu-An Chung, Mariano Coria Meglioli, David Dale, Ning Dong, Mark Duppenthaler, Paul-Ambroise Duquenne, Brian Ellis, Hady Elsahar, Justin Haaheim, John Hoffman, Min-Jae Hwang, Hirofumi Inaguma, Christopher Klaiber, Ilia Kulikov, Pengwei Li, Daniel Licht, Jean Maillard, Ruslan Mavlyutov, Alice Rakotoarison, Kaushik Ram Sadagopan, Abinesh Ramakrishnan, Tuan Tran, Guillaume Wenzek, Yilin Yang, Ethan Ye, Ivan Evtimov, Pierre Fernandez, Cynthia Gao, Prangthip Hansanti, Elahe Kalbassi, Amanda Kallet, Artyom Kozhevnikov, Gabriel Mejia, Robin San Roman, Christophe Touret, Corinne Wong, Carleigh Wood, Bokai Yu, Pierre Andrews, Can Balioglu, Peng-Jen Chen, Marta R. Costa-juss{\`a}, Maha Elbayad, Hongyu Gong, Francisco Guzm{\'a}n, Kevin Heffernan, Somya Jain, Justine Kao, Ann Lee, Xutai Ma, Alex Mourachko, Benjamin Peloquin, Juan Pino, Sravya Popuri, Christophe Ropers, Safiyyah Saleem, Holger Schwenk, Anna Sun, Paden Tomasello, Changhan Wang, Jeff Wang, Skyler Wang, Mary Williamson",
+  journal={ArXiv},
+  year={2023}
+}
+```
---
+++ predict.py
@@ -0,0 +1,224 @@
+from cog import BasePredictor, Input, Path, BaseModel
+import os
+import time
+import subprocess
+import contextlib
+import logging
+from typing import Optional
+
+import torch
+import torchaudio
+from fairseq2.data import Collater, FileMapper
+from fairseq2.data.audio import (
+    AudioDecoder,
+    WaveformToFbankConverter,
+    WaveformToFbankOutput,
+)
+
+from seamless_communication.cli.expressivity.evaluate.pretssel_inference_helper import (
+    PretsselGenerator,
+)
+from seamless_communication.cli.m4t.evaluate.evaluate import (
+    adjust_output_for_corrupted_inputs,
+)
+
+from seamless_communication.inference import (
+    SequenceGeneratorOptions,
+    BatchedSpeechOutput,
+    Translator,
+)
+from seamless_communication.models.unity import (
+    load_gcmvn_stats,
+    load_unity_unit_tokenizer,
+)
+from seamless_communication.store import add_gated_assets
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s -- %(name)s: %(message)s",
+)
+
+logger = logging.getLogger(__name__)
+
+
+LANG_TO_CODE = {
+    "English": "eng",
+    "French": "fra",
+    "Spanish": "spa",
+    "German": "deu",
+    "Italian": "ita",
+    "Chinese": "cmn",
+}
+
+MODEL_URL = "https://weights.replicate.delivery/default/SeamlessExpressive/seamless-expressive-cache.tar"
+
+
+def download_weights(url, dest):
+    start = time.time()
+    print("downloading from: ", url)
+    subprocess.check_call(["pget", "-x", url, dest])
+    print("downloading took: ", time.time() - start)
+
+
+class Output(BaseModel):
+    text_out: str
+    audio_out: Path
+
+
+class Predictor(BasePredictor):
+    def setup(self) -> None:
+        if not os.path.exists("model"):
+            download_weights(MODEL_URL, "model")
+        self.device = (
+            torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+        )
+        self.dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+        self.unit_tokenizer = load_unity_unit_tokenizer("seamless_expressivity")
+        _gcmvn_mean, _gcmvn_std = load_gcmvn_stats("vocoder_pretssel")
+        self.gcmvn_mean = torch.tensor(
+            _gcmvn_mean, device=self.device, dtype=self.dtype
+        )
+        self.gcmvn_std = torch.tensor(_gcmvn_std, device=self.device, dtype=self.dtype)
+
+        add_gated_assets(Path("./model/"))
+
+        self.mapper = FileMapper(root_dir=".", cached_fd_count=10)
+        self.decoder = AudioDecoder(dtype=torch.float32, device=self.device)
+        self.wav_to_fbank = WaveformToFbankConverter(
+            num_mel_bins=80,
+            waveform_scale=2**15,
+            channel_last=True,
+            standardize=False,
+            device=self.device,
+            dtype=self.dtype,
+        )
+
+        self.collater = Collater(pad_value=0, pad_to_multiple=1)
+        self.translator = Translator(
+            "seamless_expressivity",
+            vocoder_name_or_card=None,
+            device=self.device,
+        )
+
+        self.text_generation_opts = SequenceGeneratorOptions(
+            beam_size=5, soft_max_seq_len=(1, 200), unk_penalty=torch.inf
+        )
+        self.unit_generation_opts = SequenceGeneratorOptions(
+            beam_size=5,
+            soft_max_seq_len=(25, 50),
+        )
+
+        self.pretssel_generator = PretsselGenerator(
+            "vocoder_pretssel",
+            vocab_info=self.unit_tokenizer.vocab_info,
+            device=self.device,
+            dtype=self.dtype,
+        )
+
+    def normalize_fbank(self, data: WaveformToFbankOutput) -> WaveformToFbankOutput:
+        fbank = data["fbank"]
+        std, mean = torch.std_mean(fbank, dim=0)
+        data["fbank"] = fbank.subtract(mean).divide(std)
+        data["gcmvn_fbank"] = fbank.subtract(self.gcmvn_mean).divide(self.gcmvn_std)
+        return data
+
+    def predict(
+        self,
+        audio_input: Path = Input(
+            description="Input audio file to be translated and synthesized.",
+        ),
+        source_lang: str = Input(
+            description="The original language of your input audio.",
+            default="English",
+            choices=LANG_TO_CODE.keys(),
+        ),
+        target_lang: str = Input(
+            description="The target language for the output audio.",
+            default="French",
+            choices=LANG_TO_CODE.keys(),
+        ),
+        duration_factor: float = Input(
+            description="Recommended: 1.0 for English, Chinese, Spanish; 1.1 for German, Italian; 1.2 for French.",
+            default=1.0,
+        ),
+    ) -> Output:
+        os.system(
+            f"ffmpeg -i {audio_input} -acodec pcm_f32le -ar 16000 -ac 1 {audio_input.stem}.wav"
+        )
+        audio_input = Path(f"{audio_input.stem}.wav")
+
+        src_lang = LANG_TO_CODE[source_lang]
+        tgt_lang = LANG_TO_CODE[target_lang]
+        print("Preparing to convert from", source_lang, "to", target_lang)
+
+        fbank = self.collater(
+            self.normalize_fbank(
+                self.wav_to_fbank(self.decoder(self.mapper(audio_input)["data"]))
+            )
+        )
+
+        output_path = Path("./tmp") / audio_input.stem
+        output_path.mkdir(parents=True, exist_ok=True)
+
+        waveforms_dir = output_path / "waveform"
+        waveforms_dir.mkdir(parents=True, exist_ok=True)
+
+        hyps = []
+
+        with contextlib.ExitStack() as stack:
+
+            valid_sequences: Optional[torch.Tensor] = None
+            source = fbank["fbank"]
+
+            # Skip corrupted audio tensors.
+            valid_sequences = ~torch.any(
+                torch.any(torch.isnan(source["seqs"]), dim=1), dim=1
+            )
+
+            if not valid_sequences.all():
+                logger.warning(f"Sample has some corrupted input.")
+                source["seqs"] = source["seqs"][valid_sequences]
+                source["seq_lens"] = source["seq_lens"][valid_sequences]
+
+            # Skip performing inference when the input is entirely corrupted.
+            if source["seqs"].numel() > 0:
+                prosody_encoder_input = fbank["gcmvn_fbank"]
+                text_output, unit_output = self.translator.predict(
+                    source,
+                    "s2st",
+                    tgt_lang,
+                    src_lang=src_lang,
+                    text_generation_opts=self.text_generation_opts,
+                    unit_generation_opts=self.unit_generation_opts,
+                    unit_generation_ngram_filtering=False,
+                    duration_factor=duration_factor,
+                    prosody_encoder_input=prosody_encoder_input,
+                )
+
+                assert unit_output is not None
+                speech_output = self.pretssel_generator.predict(
+                    unit_output.units,
+                    tgt_lang=tgt_lang,
+                    prosody_encoder_input=prosody_encoder_input,
+                )
+
+            else:
+                text_output = []
+                speech_output = BatchedSpeechOutput(units=[], audio_wavs=[])
+
+            if valid_sequences is not None and not valid_sequences.all():
+                text_output, speech_output = adjust_output_for_corrupted_inputs(  # type: ignore[assignment]
+                    valid_sequences,
+                    text_output,
+                    speech_output,
+                )
+
+            hyps += [str(s) for s in text_output]
+
+            text = text_output[0]
+            torchaudio.save(
+                waveforms_dir / f"output.wav",
+                speech_output.audio_wavs[0][0].to(torch.float32).cpu(),
+                sample_rate=speech_output.sample_rate,
+            )
+            return Output(text_out=str(text), audio_out=waveforms_dir / f"output.wav")
